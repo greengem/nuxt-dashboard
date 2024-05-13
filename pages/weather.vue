55b9a281b60eec44c34e798eb31348a83a8ce8b3
Green Gem <chris_waitt@me.com>
2024-05-13 18:05:50 +0100

    refactor: Update weather page to display detailed weather information

--- pages/weather.vue
+++ pages/weather.vue
@@ -1,12 +1,16 @@
 <template>
   <RouteHeading>Weather</RouteHeading>
-  <h1>Weather Near You</h1>
   <div v-if="loading">
     Loading weather data...
   </div>
   <div v-else-if="weather">
-    <pre>{{ jsonStringifiedWeather }}</pre>
+    <p>Location: {{ weather.name }}</p>
     <p>Temperature: {{ weather.main.temp }}°C</p>
+    <p>Feels Like: {{ weather.main.feels_like }}°C</p>
+    <p>Min: {{ weather.main.temp_min }}°C</p>
+    <p>Max: {{ weather.main.temp_max }}°C</p>
+    <p>Pressure: {{ weather.main.pressure }}</p>
+    <p>Humidity: {{ weather.main.humidity }}</p>
     <p>Weather condition: {{ weather.weather[0].description }}</p>
   </div>
   <div v-else>
@@ -14,33 +18,58 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 
-const weather = ref(null);
+interface Weather {
+  name: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  weather: {
+    description: string;
+  }[];
+}
+
+const weather = ref<Weather | null>(null);
 const loading = ref(false);
-const error = ref(null);
+const error = ref<string | null>(null);
+
+const locationDebugEnabled = true;
+const locationDebug = 'London,UK';
 
 onMounted(async () => {
   loading.value = true;
-  navigator.geolocation.getCurrentPosition(async (position) => {
-    const { latitude, longitude } = position.coords;
-    const location = `${latitude},${longitude}`;
+  if (locationDebugEnabled) {
     try {
-      weather.value = await $fetch(`/api/weather?location=${location}`);
+      weather.value = await $fetch(`/api/weather?location=${locationDebug}`);
     } catch (err) {
       error.value = 'Failed to load weather data';
       console.error(err);
     } finally {
       loading.value = false;
     }
-  }, (geoError) => {
-    error.value = 'Geolocation is not supported by this browser.';
-    console.error(geoError);
-    loading.value = false;
-  });
-});
-
-const jsonStringifiedWeather = computed(() => {
-  return weather.value ? JSON.stringify(weather.value, null, 2) : '{}';
+  } else {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const { latitude, longitude } = position.coords;
+      const location = `${latitude},${longitude}`;
+      try {
+        weather.value = await $fetch(`/api/weather?location=${location}`);
+      } catch (err) {
+        error.value = 'Failed to load weather data';
+        console.error(err);
+      } finally {
+        loading.value = false;
+      }
+    }, (geoError) => {
+      error.value = 'Geolocation is not supported by this browser.';
+      console.error(geoError);
+      loading.value = false;
+    });
+  }
 });
 </script>
