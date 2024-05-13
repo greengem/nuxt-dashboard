ccb84449c9b489e6541f7bb4b31af321a3da3d3a
Green Gem <chris_waitt@me.com>
2024-05-13 17:10:11 +0100

    refactor: Update Navbar component to use NuxtLink instead of anchor tags

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,21 @@
 <template>
-  <nav class="bg-ctp-crust flex items-center justify-between h-16 text-ctp-text">
-      <a href="#" class="text-2xl tracking-tight font-light text-ctp-mauve mx-5">ACME.TM</a>
-      <div class="p-5 flex items-center">
-        <img 
-          v-if="loggedIn && data && data.user" 
-          :src="data.user?.image || 'fallback-image-url'" 
-          alt="User Avatar" 
-          class="h-8 w-8 rounded-full ring-2 ring-offset-2 ring-offset-ctp-crust ring-ctp-mauve"
-        >
-        <button v-else @click="handleSignIn">Sign In</button>
-      </div>
+  <nav class="bg-ctp-crust flex items-center justify-between h-16 text-ctp-text border-b-2 border-ctp-mauve">
+
+    <div class="flex items-center gap-2 mx-4 text-ctp-mauve">
+      <Icon name="tabler:tools" size="28" />
+      <NuxtLink href="#" class="text-2xl tracking-tight font-light">ACME.TM</NuxtLink>
+    </div>
+
+    <div class="p-5 flex items-center">
+      <img 
+        v-if="loggedIn && data && data.user" 
+        :src="data.user?.image || 'fallback-image-url'" 
+        alt="User Avatar" 
+        class="h-8 w-8 rounded-full ring-2 ring-offset-2 ring-offset-ctp-crust ring-ctp-mauve"
+      >
+      <button v-else @click="handleSignIn">Sign In</button>
+    </div>
+
   </nav>
 </template>
 
