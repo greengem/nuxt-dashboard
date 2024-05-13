66c3c52e75a24d71daf92559cf05f103079ec150
Green Gem <chris_waitt@me.com>
2024-05-13 18:05:43 +0100

    refactor: Update GitHubLogin component to use Tabler icon library

--- components/GitHubLogin.vue
+++ components/GitHubLogin.vue
@@ -1,6 +1,6 @@
 <template>
     <button @click="$event => signIn('github')" class="bg-ctp-mauve text-ctp-base flex items-center gap-2 px-4 py-2 rounded-lg text-sm">
-        <IconBrandGithub size="18" stroke="1.5" />
+        <Icon name="tabler:brand-github" size="18" stroke="1.5" />
         <span>Sign in with GitHub</span>
     </button>
 </template>
