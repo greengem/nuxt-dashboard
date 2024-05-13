dcaa86850b74e5b25857a46d0496b868c30d29a2
Green Gem <chris_waitt@me.com>
2024-05-13 14:18:49 +0100

    chore: Add GitHub authentication provider to NuxtAuthHandler

---
+++ server/api/auth/[...].ts
@@ -0,0 +1,18 @@
+// file: ~/server/api/auth/[...].ts
+import GithubProvider from 'next-auth/providers/github'
+import { NuxtAuthHandler } from '#auth'
+
+export default NuxtAuthHandler({
+  // A secret string you define, to ensure correct encryption
+  secret: process.env.AUTH_SECRET,
+  providers: [
+    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
+    GithubProvider.default({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    })
+  ],
+  pages: {
+    signIn: '/login',
+  },
+})
