f11dc93ac1bf42fdc754840051ecc6aee0ef1686
Green Gem <chris_waitt@me.com>
2024-05-13 11:52:04 +0100

    Add NuxtLayout, Meta and some tailwind classes to the body

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    
-  </div>
-  <NuxtPage />
+  <NuxtLayout>
+    <Navbar />
+    <div class="p-5">
+      <NuxtPage />
+    </div>
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+  useHead({
+    title: 'Nuxt Personal Dashboard',
+    meta: [
+      { name: 'description', content: 'A personal dashboard to monitor all your things' }
+    ],
+    bodyAttrs: {
+      class: 'min-h-dvh flex flex-col grow bg-ctp-base'
+    }
+  })
+</script>
