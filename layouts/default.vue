db4cf997631c50f1ffa204c30b2881dc38f0b5f6
Green Gem <chris_waitt@me.com>
2024-05-13 11:55:58 +0100

    Default Template for generic pages

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex grow text-ctp-text relative">
+        <Sidebar />
+        <main class="flex flex-col grow ml-16">
+            <slot />
+        </main>
+    </div>
+</template>
