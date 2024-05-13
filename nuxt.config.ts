9b0ef0eacb945a1ff69c97cc4f49a240062e9193
Green Gem <chris_waitt@me.com>
2024-05-13 18:03:22 +0100

    chore: Update dependencies and add new modules for TailwindCSS, authentication, icons, image handling, and Google Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@sidebase/nuxt-auth", "nuxt-icon"],
+  modules: [
+    "@nuxtjs/tailwindcss", 
+    "@sidebase/nuxt-auth", 
+    "nuxt-icon", 
+    "@nuxt/image", 
+    "@nuxtjs/google-fonts"
+  ],
+  googleFonts: {
+    families: {
+      Poppins: true
+    }
+  },
   auth: {
     globalAppMiddleware: true,
     provider: {
@@ -17,5 +28,6 @@ export default defineNuxtConfig({
   runtimeConfig: {
     openWeatherApiKey: process.env.OPENWEATHER_API_KEY,
     windyMapApiKey: process.env.WINDY_MAP_API_KEY,
+    newsApiKey: process.env.NEWS_API_KEY,
   }
 })
