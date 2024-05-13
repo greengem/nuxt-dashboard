14f4c8ff3359dde60275ea4c82dd8d2cfc44b2f5
Green Gem <chris_waitt@me.com>
2024-05-13 15:14:50 +0100

    feat: Add login page with GitHub authentication button

---
+++ pages/login.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+definePageMeta({
+    auth: {
+        unauthenticatedOnly: true,
+        navigateAuthenticatedTo: '/dashboard',
+    }
+})
+
+</script>
+
+<template>
+    <div class="bg-ctp-surface0 p-5 rounded-lg max-w-96 mx-auto">
+        <h1 class="text-3xl">Login</h1>
+        <hr class="my-3" />
+        <GitHubLogin />
+    </div>
+
+</template>
